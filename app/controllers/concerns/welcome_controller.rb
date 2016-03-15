6732de3f47c8011b8e67c6e1489f0244d65d64f6
robinroyer <robin.royer974@gmail.com>
2016-03-15 20:01:55 +0100

    wip

---
+++ app/controllers/concerns/welcome_controller.rb
@@ -0,0 +1,9 @@
+class WelcomeController < ActionController::Base
+
+
+  def index
+    render json: {
+      :connexion => [:lien => '/users/sign_in(.:format)', :methode => 'Post']
+    }
+  end
+end
