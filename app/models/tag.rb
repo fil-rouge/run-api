6d7bfb255d112e63a52e6b41ed7ffcb64dcb085f
robinroyer <robin.royer974@gmail.com>
2016-03-25 18:53:43 +0100

    staging error for a while

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,8 +1,8 @@
 class Tag < ActiveRecord::Base
   belongs_to :parcour
 
-  validates :value
-            :presence => true,
-            :length => { :maximum => 50 },
-            :uniqueness => true
+  # validates :value
+  #           :presence => true,
+  #           :length => { :maximum => 50 },
+  #           :uniqueness => true
 end
