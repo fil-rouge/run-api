879d4ff5f93355ec0b03beb2ff2f89fd98c738e1
robinroyer <robin.royer974@gmail.com>
2016-04-09 00:03:30 +0200

    add user#destroy

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,8 @@ class UsersController < AuthenticateController
   end
 
   def index
-
+    @userCount = User.count
+    render json: {:data => @userCount}
   end
 
   def edit
@@ -30,7 +31,19 @@ class UsersController < AuthenticateController
   end
 
   def destroy
+    user_id = params[:id].presence.to_i
 
+    #  check if you ask informations about yourself
+    if current_user.id == user_id
+      @user = User.find_by_id(user_id)
+      if @user.delete
+        render json: {:data => @user}
+      else
+        render json: {:error => 'an error occured, the user is not deleted'}
+    end
+    else
+      render json: {:error => 'We are sorry, you can not delete an other user than yourself'}
+    end
   end
 
 
