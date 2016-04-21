9a9f6c318326017849f7654ceda8e06f2ba17913
robinroyer <robin.royer974@gmail.com>
2016-04-21 19:03:15 +0400

    adding helper and model rspec test

---
+++ spec/helpers/errors_helper_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+# Specs in this file have access to a helper object that includes
+# the CircuitsHelper. For example:
+#
+# describe CircuitsHelper do
+#   describe "string concat" do
+#     it "concats two strings with spaces" do
+#       expect(helper.concat_strings("this","that")).to eq("this that")
+#     end
+#   end
+# end
+RSpec.describe CircuitsHelper, type: :helper do
+
+  describe "json_error" do
+
+    it "get an error response well format" do
+      expect false
+    end
+
+    it "check if all the error are registered" do
+      expect false
+    end
+  end
+end
--- spec/models/users_spec.rb
+++ spec/models/users_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  # user = FactoryGirl::build(:user)
+  # ========================== SET_AUTH_TOKEN =========================
+  describe "set_auth_token" do
+    it "gets a auth_token" do
+      expect false
+    end
+
+    it "doesn't take a auth token" do
+      expect false
+    end
+  end
+
+  # ========================== GENERATE_AUTHENTICATION_TOKEN =========================
+  describe "generate_authentication_token" do
+    it "generate an authentication token" do
+      expect false
+    end
+  end
+
 end
