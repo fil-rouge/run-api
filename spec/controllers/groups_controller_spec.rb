890edfa5af84f78103c4b6963b74fe9f2d0583b9
robinroyer <robin.royer974@gmail.com>
2016-04-21 18:38:04 +0400

    begin to implement rspec test

--- spec/controllers/groups_controller_spec.rb
+++ spec/controllers/groups_controller_spec.rb
@@ -2,4 +2,20 @@ require 'rails_helper'
 
 RSpec.describe GroupsController, type: :controller do
 
+  describe "index" do
+    before do
+      # TODO: use factorygirl her
+
+    end
+
+    it "get a well form  response" do
+      @group = get :index
+      expect 1 == 2
+    end
+
+    it "get all the groups" do
+      @group = get :index
+      expect 1 == 2
+    end
+
 end
