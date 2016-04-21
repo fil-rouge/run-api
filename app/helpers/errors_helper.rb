9d64c972914ab0e3acda61bf2f4ef088c9be8c65
robinroyer <robin.royer974@gmail.com>
2016-04-21 18:25:01 +0400

    creating an helper for error mesaage

---
+++ app/helpers/errors_helper.rb
@@ -0,0 +1,17 @@
+module ErrorsHelper
+  # TODO : continue
+  ERROR = {
+    :invalid_token => "Invalid authorization: your token is not valid ",
+    :invalid_header => "Invalid authorization: you need to give the user id and the authenthification token in the header as HTTP_USER_ID and HTTP_TOKEN",
+    :unknown_error => "An unknown error has occured"
+  }
+
+# TODO: check how to return the status code in the json or in the request
+  def json_error(symbol = :unknown_error, code = 401  )
+    {:error => ERROR[symbol] ) #, :status => code}
+  end
+
+
+
+
+end
