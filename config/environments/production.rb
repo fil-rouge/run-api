5764e2e0225f20563c4f4c5c62e62e6bc908a1ea
robinroyer <robin.royer974@gmail.com>
2016-04-24 20:35:01 +0200

    adding mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,6 +24,24 @@ Rails.application.configure do
   # Apache or NGINX already handles this.
   config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
+  config.action_mailer.default_url_options = { :host => 'orunginal-api.herokuapp.com' }
+  Rails.application.routes.default_url_options[:host] = 'orunginal-api.herokuapp.com'
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default :charset => "utf-8"
+
+  config.action_mailer.smtp_settings = {
+  address: "smtp.gmail.com",
+  port: 587,
+  domain: ENV["GMAIL_DOMAIN"],
+  authentication: "plain",
+  enable_starttls_auto: true,
+  user_name: ENV["GMAIL_USERNAME"],
+  password: ENV["GMAIL_PASSWORD"]
+  }
+
   # Compress JavaScripts and CSS.
   # config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
