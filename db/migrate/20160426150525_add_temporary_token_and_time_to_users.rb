7824c294f10a62492e1e6bd9dcc2ee08dedaed1e
robinroyer <robin.royer974@gmail.com>
2016-04-26 18:47:44 +0200

    wip

--- db/migrate/20160426150525_add_temporary_token_and_time_to_users.rb
+++ db/migrate/20160426150525_add_temporary_token_and_time_to_users.rb
@@ -1,6 +1,6 @@
 class AddTemporaryTokenAndTimeToUsers < ActiveRecord::Migration
   def change
-    add_column :users, :temporary_token, :string, null: false, default: ""
+    add_column :users, :temporary_token, :string, null: false, default: Devise.friendly_token
     add_column :users, :previous_temporary_token_date, :datetime, null: false, default: DateTime.now
   end
 end
